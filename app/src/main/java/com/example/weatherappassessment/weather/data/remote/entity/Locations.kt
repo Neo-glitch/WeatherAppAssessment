1237f73089ec6c4de9ea9eb07d7c412b98f47e02
Nweke Onyekachukwu <55864433+Neo-glitch@users.noreply.github.com>
2024-09-10 21:50:13 +0100

    setup infrastructure for getting remote data

---
+++ app/src/main/java/com/example/weatherappassessment/weather/data/remote/entity/Locations.kt
@@ -0,0 +1,14 @@
+package com.example.weatherappassessment.weather.data.remote.entity
+
+data class Locations(
+	val locations: List<Location>? = null
+)
+
+data class Location(
+	val country: String? = null,
+	val name: String? = null,
+	val lon: Double? = null,
+	val state: String? = null,
+	val lat: Double? = null,
+)
+
---
+++ app/src/main/java/com/example/weatherappassessment/weather/data/remote/entity/WeatherResponse.kt
@@ -0,0 +1,63 @@
+package com.example.weatherappassessment.weather.data.remote.entity
+
+data class WeatherResponse(
+	val city: City? = null,
+	val cnt: Int? = null,
+	val cod: String? = null,
+	val message: Double? = null,
+	val list: List<WeatherItem>? = null
+)
+
+data class WeatherObject(
+	val icon: String? = null,
+	val description: String? = null,
+	val main: String? = null,
+	val id: Int? = null
+)
+
+data class Temp(
+	val min: Double? = null,
+	val max: Double? = null,
+	val eve: Double? = null,
+	val night: Double? = null,
+	val day: Double? = null,
+	val morn: Double? = null
+)
+
+data class Coord(
+	val lon: Double? = null,
+	val lat: Double? = null
+)
+
+data class WeatherItem(
+	val sunrise: Int? = null,
+	val temp: Temp? = null,
+	val deg: Int? = null,
+	val pressure: Int? = null,
+	val clouds: Int? = null,
+	val feelsLike: FeelsLike? = null,
+	val speed: Double? = null,
+	val dt: Int? = null,
+	val pop: Int? = null,
+	val sunset: Int? = null,
+	val weather: List<WeatherObject>? = null,
+	val humidity: Int? = null,
+	val gust: Double? = null
+)
+
+data class City(
+	val country: String? = null,
+	val coord: Coord? = null,
+	val timezone: Int? = null,
+	val name: String? = null,
+	val id: Int? = null,
+	val population: Int? = null
+)
+
+data class FeelsLike(
+	val eve: Double? = null,
+	val night: Double? = null,
+	val day: Double? = null,
+	val morn: Double? = null
+)
+
